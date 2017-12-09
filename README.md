c511cc41f0b76b4ae454b57af697384751b4fd41
noelportugal <noel.portugal@oracle.com>
2017-12-08 21:14:42 -1000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # alexa-reminders-listener
+
+This is an example of using [https://github.com/noelportugal/alexa-reminders](https://github.com/noelportugal/alexa-reminders) as a listener.
+
+#### Listener
+
+```sh
+$ git clone https://github.com/noelportugal/alexa-reminders-listener
+$ cd alexa-reminders-listener
+$ npm install
+$ node index.js
+External endpoint:
+ curl -X POST -d "reminder=Ask Alexa team for a proper Reminders API" https://xxxxxx.ngrok.io/alexa-reminders
+
+Internal endpoint:
+ curl -X POST -d "reminder=Ask Alexa team for a proper Reminders API" http://localhost:8091/alexa-reminders
+Logged in
+
+```
---
+++ index.js
@@ -0,0 +1,29 @@
+var reminders = require('alexa-reminders')
+var express = require('express')
+var ngrok = require('ngrok')
+var bodyParser = require('body-parser')
+var app = express()
+const serverPort = 8091
+var savedConfig = {}
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+reminders.login('Device Name', 'username', 'password', function(error, response, config){
+  savedConfig = config
+  console.log(response)
+})
+
+app.post('/alexa-reminders', urlencodedParser, function (req, res) {
+  var reminder = req.body.reminder;
+  reminders.setReminder(reminder, null, savedConfig, function(error, response){
+    res.send(response)
+  })
+})
+
+app.listen(serverPort, function () {
+  ngrok.connect(serverPort, function (err, url) {
+    console.log('External endpoint:')
+    console.log('curl -X POST -d "reminder=Ask Alexa team for a proper Reminders API" ' + url + '/alexa-reminders\n')
+    console.log('Internal endpoint:')
+    console.log('curl -X POST -d "reminder=Ask Alexa team for a proper Reminders API" ' + 'http://localhost:' + serverPort + '/alexa-reminders')
+  })
+})
